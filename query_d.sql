62408287559a0b15b4ae8982797a3f45205b4646
Tomas <103332587+tomasfgonzalez@users.noreply.github.com>
2023-06-19 14:51:27 -0300

    Add files via upload

--- query_d.sql
+++ query_d.sql
@@ -1,7 +1,8 @@
 SELECT first_name
-  FROM people
+  FROM people as p
  WHERE NOT EXISTS
        (SELECT *
           FROM frequents as f, likes_bar as l
          WHERE f.person_id = l.person_id
-           AND f.bar_id = l.bar_id);
+           AND f.bar_id = l.bar_id
+       		AND p.id =f.person_id);
